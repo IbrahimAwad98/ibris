815e8e102e727c86a0ea3768fc6257ad36edabae
IbrahimAwad98 <193368218+IbrahimAwad98@users.noreply.github.com>
2026-07-31 10:32:41 +0200

    feat: tab bar shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { TabBar } from "./features/shell/TabBar";
 import { PdfViewer } from "./features/viewer/PdfViewer";
 import "./App.css";
 
 function App() {
-  return <PdfViewer />;
+  return (
+    <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+      <TabBar />
+      <div style={{ flex: 1, minHeight: 0 }}>
+        <PdfViewer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
